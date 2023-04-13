2010dad478ab0253ceb4ebf70abf8ae0caec16c2
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-04-13 19:38:19 +0300

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :jobseekerprofiles
-  resources :employerprofiles
-  resources :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
