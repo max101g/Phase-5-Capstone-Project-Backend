47d50f308c1c01a7404df3d071cbc457ed41072f
steve <austines811@gmail.com>
2023-04-14 17:46:50 +0300

    adds custom verification routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-  resources :jobs
-  resources :jobtags
-  resources :users
-  resources :seekers
-  resources :employers
   resources :admins
+  resources :employers
+  resources :seekers
+  resources :users
+  resources :jobtags
+  resources :jobs
+  resources :posts  
+  resources :comments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # verify profiles
+  patch "/employers/:id/verify",to: "employers#verify"
+  patch "/seekers/:id/verify", to: "seekers#verify"
 
   # Defines the root path route ("/")
   # root "articles#index"
