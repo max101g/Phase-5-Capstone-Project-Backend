8ea9966a2a371ba2ec46bc8ed718e46a55a7a846
steve <austines811@gmail.com>
2023-04-15 16:23:58 +0300

    implements role based action access, omitting elavated role req action

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,14 +1,11 @@
 class EmployersController < ApplicationController
+    before_action :authenticate_user
+    before_action :role_auth
     def create
         employer = Employer.create(employer_code: generate_code(12), company_name: params[:company_name], email: params[:email], verified: params[:verified], user_code: params[:user_code])
         render json: employer, status: :created
-    end 
+    end   
     
-    def index
-        employers = Employer.all
-        render json: employers, status: :ok
-    end
-
     def show
         employer = find_employer
         render json: employer, status: :ok
@@ -18,13 +15,7 @@ class EmployersController < ApplicationController
         employer = find_employer
         employer.update(employer_params)
         render json: employer, status: :ok
-    end
-
-    def verify
-        employer = find_employer
-        employer.update(verified: params[:verified])
-        render json: employer, status: :ok
-    end
+    end    
     
     def destroy
         employer = find_employer
@@ -33,11 +24,28 @@ class EmployersController < ApplicationController
     end
 
     private
+
     def find_employer
         Employer.find(params[:id])
     end
 
     def employer_params
         params.permit(:company_name, :email, :location, :avatar, :description, :verified)
+    end
+
+    def role_auth
+        # Get token from request headers
+        role = ["EMPLOYER", "ADMIN"]
+        header = request.headers['Authorization']
+        token = header.split(' ').last if header
+        if token
+            begin
+                decoded_token = JWT.decode(token, Rails.application.secret_key_base)
+                user_role = decoded_token[0]['role']
+                unless  role.include?(user_role)
+                    render json: {error: 'Unauthorized To Access Resource'}, status: :unauthorized
+                end
+            end
+        end
     end
 end
